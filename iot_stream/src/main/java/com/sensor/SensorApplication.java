03f65a96f2f83d287ca4f00b24decea30e4c912d
Noah <noah.a.stuart@gmail.com>
2023-10-10 21:00:21 -0400

    Update Project Structure

--- iot_stream/src/main/java/com/sensor/SensorApplication.java
+++ iot_stream/src/main/java/com/sensor/SensorApplication.java
@@ -1,4 +1,5 @@
 package com.sensor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
